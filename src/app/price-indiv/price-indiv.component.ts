ac8bcd231f62d217f5671d9a22e8f14b636464a8
Maksym Oliynik <maksymoliynik@gmail.com>
2024-01-15 12:44:14 +0200

    first commit

---
+++ src/app/price-indiv/price-indiv.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-indiv',
+  templateUrl: './price-indiv.component.html',
+  styleUrls: ['./price-indiv.component.scss']
+})
+export class PriceIndivComponent {
+
+}
---
+++ src/app/price-online/price-online.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-online',
+  templateUrl: './price-online.component.html',
+  styleUrls: ['./price-online.component.scss']
+})
+export class PriceOnlineComponent {
+
+}
